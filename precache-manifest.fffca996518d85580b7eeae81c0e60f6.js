ec0add0726f759cab7ff566bf42038d55fa3e60b
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-06-20 12:22:37 +0530

    Updates

---
+++ precache-manifest.fffca996518d85580b7eeae81c0e60f6.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fffb17d64d44f519a2a1130ee0a3f7d",
+    "url": "/truvisory/index.html"
+  },
+  {
+    "revision": "6064f29e11c6cd8730e1",
+    "url": "/truvisory/static/css/main.901c6f62.chunk.css"
+  },
+  {
+    "revision": "e492de5ab0320c1ae647",
+    "url": "/truvisory/static/js/2.28e30e0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/truvisory/static/js/2.28e30e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6064f29e11c6cd8730e1",
+    "url": "/truvisory/static/js/main.b6a06f1f.chunk.js"
+  },
+  {
+    "revision": "5920dbbffb0f79cfd538",
+    "url": "/truvisory/static/js/runtime-main.3a598432.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/truvisory/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/truvisory/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/truvisory/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/truvisory/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/truvisory/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/truvisory/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/truvisory/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/truvisory/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/truvisory/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/truvisory/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/truvisory/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/truvisory/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/truvisory/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/truvisory/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/truvisory/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/truvisory/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/truvisory/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/truvisory/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/truvisory/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/truvisory/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/truvisory/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/truvisory/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/truvisory/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/truvisory/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/truvisory/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/truvisory/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3bafd080c7fdce65718dd2749ff7e28c",
+    "url": "/truvisory/static/media/website_background.3bafd080.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Truvisory/precache-manifest.203a9661280d937d4e8432dd48702997.js"
+  "/truvisory/precache-manifest.fffca996518d85580b7eeae81c0e60f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Truvisory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/truvisory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
